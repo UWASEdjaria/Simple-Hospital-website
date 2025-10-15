955bb27c117f9dd3f0665c0b950e1409c8a1a8cf
Uwase Djaria <uwasedjaria41@gmail.com>
2025-10-15 21:21:41 +0200

    add home page with style and style navbar and add picture

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
+     <div className="flex flex-col items-center justify-center w-full h-screen bg-cover bg-center text-white p-8"
+         style={{
+           backgroundImage: "url('hospital.jpg')",
+           backgroundSize: 'cover',
+           backgroundPosition: 'center',  
+         }}>
+      <section>
+      <h1 className=" text-white font-bold text-5xl sm:text-2xl md:text-4xl lg:text-5xl mb-5">Welcome to HopeCare Hospital</h1>
+      <p className=' text-white text-2xl leading-relaxed m-auto'> We are committed to providing professional 
+        and affordable healthcare services<br></br> 
+      for you and your family. Experience trusted medical care with modern technology <br></br>
+      and compassionate doctors.</p>
+      <button className='rounded-lg bg-blue-600 hover:to-blue-900 transition-all duration-300 hover:scale-105 m-5 p-2'>Book an Appointment</button>
+      </section>
       
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,15 @@ import {Link} from "react-router-dom"
 
  function Navbar() {
   return (
-    <div>
-       <nav>
+    <span  className='flex justify-between items-center  bg-white text-black gap-4 m-5 rounded-lg shadow-lg space-x-4 p-4 '>
+      <h1 className='text-xl text-blue-600 italic  hover:underline transition-all duration-300 hover:scale-105'>Health Services</h1>
+       <nav className='italic hover:underline transition-all duration-300 hover:scale-105 m-2 space-x-4'>
        <Link to="/">Home</Link>
        <Link to="/about">About</Link>
        <Link to="/contact">Contact</Link>
        <Link to="/services">Services</Link>
        <Link to="/doctors">Doctors</Link>
      </nav>
-    </div>
+    </span>
   )
 }export default Navbar
